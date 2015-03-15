42990c059e64035cef81c63c427630d038e44001
Aldric Giacomoni <trevoke@gmail.com>
2015-03-14 21:43:28 -0400

    Oh god the large commits

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'How playing Go will make you a better Agilist', 
+     subtitle: 'Weiqi: The perfect abstraction', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: false, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/emacs-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Aldric Giacomoni', 
+     company: 'Stride NYC', 
+     gplus: 'https://plus.google.com/+AldricGiacomoni', 
+     twitter: 'Trevoke', 
+     www: 'http://blog.trevoke.net', 
+     github: 'http://github.com/Trevoke', 
+   }]
+};
